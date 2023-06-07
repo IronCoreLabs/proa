c33693277121e6e91bd4434c53de111e1d869886
Chris Jones <chris.jones@ironcorelabs.com>
2023-06-07 10:31:09 -0600

    Remove inner_main.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::process::ExitCode;
 
+use anyhow::Error;
 use clap::Parser;
 use config::Cli;
 use tracing::{info, warn};
@@ -11,29 +12,20 @@ mod shutdown;
 mod stream;
 
 #[tokio::main]
-async fn main() -> ExitCode {
+async fn main() -> Result<ExitCode, Error> {
     let cli = Cli::parse();
 
     tracing_subscriber::fmt().json().init();
     info!("Starting up.");
 
-    let status = inner_main(cli).await.unwrap_or_else(|e| { 
-        warn!(error = error.to_string());
-        1
-    });
-    info!(status, "Exiting.");
-    status.into()
-}
-
-/// Convenience function so we can return a Result.
-async fn inner_main(cli: Cli) -> Result<u8, anyhow::Error> {
     let pod = k8s::wait_for_ready().await?;
 
-    let result = exec::run(&cli.command, &cli.args);
+    let status = exec::run(&cli.command, &cli.args);
 
     if let Err(err) = shutdown::shutdown(cli, pod).await {
         warn!(err = err.to_string(), "Shutdown problem");
     }
 
-    result
+    info!(?status, "Exiting.");
+    status.map(|c| c.into())
 }
